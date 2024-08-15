8751a5e51a28af26c4547610468b2b2e81ecd8d5
catetag <101448571+catetag@users.noreply.github.com>
2024-08-15 16:20:19 +0300

    countries controller'ı temizlendi

--- MyPage/Controllers/CountriesController.cs
+++ MyPage/Controllers/CountriesController.cs
@@ -13,26 +13,7 @@ namespace MyPage.Controllers
     
     public class CountriesController : BaseController
     {
-        [HttpPost]
-        public ActionResult SubmitEmail(string email, string password)
-        {
-            // Basit bir kontrol yapalım. Bu, genellikle veritabanı veya başka bir hizmetle yapılır.
-            // Örnek olarak sabit bir email ve şifre kontrolü yapıyoruz.
-            string correctEmail = "test";
-            string correctPassword = "test";
-
-            if (email == correctEmail && password == correctPassword)
-            {
-                // Eğer email ve şifre doğruysa, kullanıcıyı bir başka sayfaya yönlendiriyoruz.
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                // Eğer email veya şifre yanlışsa, hata mesajı gösteriyoruz.
-                ViewBag.ErrorMessage = "Email veya şifre yanlış!";
-                return View();
-            }
-        }
+       
         public ActionResult login()
         {
             return View();
@@ -60,84 +41,84 @@ namespace MyPage.Controllers
         }
 
         // GET: Countries/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
         // POST: Countries/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "PKID,country_2_digit,country_3_digit,country_numeric_code,ui_language,Name_English,Name_Turkce")] Countries countries)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Countries.Add(countries);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "PKID,country_2_digit,country_3_digit,country_numeric_code,ui_language,Name_English,Name_Turkce")] Countries countries)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Countries.Add(countries);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
 
-            return View(countries);
-        }
+        //    return View(countries);
+        //}
 
         // GET: Countries/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Countries countries = db.Countries.Find(id);
-            if (countries == null)
-            {
-                return HttpNotFound();
-            }
-            return View(countries);
-        }
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Countries countries = db.Countries.Find(id);
+        //    if (countries == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(countries);
+        //}
 
         // POST: Countries/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "PKID,country_2_digit,country_3_digit,country_numeric_code,ui_language,Name_English,Name_Turkce")] Countries countries)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(countries).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(countries);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "PKID,country_2_digit,country_3_digit,country_numeric_code,ui_language,Name_English,Name_Turkce")] Countries countries)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(countries).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(countries);
+        //}
 
         // GET: Countries/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Countries countries = db.Countries.Find(id);
-            if (countries == null)
-            {
-                return HttpNotFound();
-            }
-            return View(countries);
-        }
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Countries countries = db.Countries.Find(id);
+        //    if (countries == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(countries);
+        //}
 
-        // POST: Countries/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Countries countries = db.Countries.Find(id);
-            db.Countries.Remove(countries);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //// POST: Countries/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Countries countries = db.Countries.Find(id);
+        //    db.Countries.Remove(countries);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
         protected override void Dispose(bool disposing)
         {
